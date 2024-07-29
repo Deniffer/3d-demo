e3e0d64476da8da171cbf7d382a4926102167441
Deniffer <lovewyqforever@gmail.com>
2024-07-29 19:19:56 +0800

    fix: build error

--- src/components/SingleModel.tsx
+++ src/components/SingleModel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useCallback } from "react";
+import React, { useEffect, useRef } from "react";
 import { useThree, useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import * as THREE from "three";
